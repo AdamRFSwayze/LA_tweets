1ccee0038e08d24fa1b94b2cc1c8f2f8ce05f470
adamrfswayze <adamrfswayze@gmail.com>
2017-02-19 16:47:20 -0600

    Fixed socket error

--- tweets_SQL.R
+++ tweets_SQL.R
@@ -3,4 +3,11 @@
 library(RMySQL)
 
 
-mydb= dbConnect(MySQL(), user='root',password='sql', dbname='hello', host='localhost')
+#To find the socket location, type ""netstat -ln | awk '/mysql(.*)?\.sock/ { print $9 }'"" in terminal
+
+
+mydb= dbConnect(MySQL(), user='root',password='sql', dbname='hello', host='localhost', unix.socket="/var/run/mysqld/mysqld.sock")
+
+dbListTables(mydb)
+
+#Use https://www.r-bloggers.com/accessing-mysql-through-r/
